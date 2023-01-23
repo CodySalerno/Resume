f4d99a58d3779ff716fc84a0469607c3fee70afe
CodySalerno <114028734+CodySalerno@users.noreply.github.com>
2023-01-23 00:23:47 -0600

    made header and about section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { WorkComponent } from './work/work.component';
+import { DemosComponent } from './demos/demos.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    AboutComponent,
+    ExperienceComponent,
+    WorkComponent,
+    DemosComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
